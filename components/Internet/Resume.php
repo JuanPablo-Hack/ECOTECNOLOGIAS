8ecc07641d77df72e4fabfd036f269f2f4e0a7a9
Ing. Juan Pablo Figueroa Jaramillo <juanpablodejesusfigueroa@gmail.com>
2024-03-26 12:55:41 -0600

    Subiendo todo el sitio

--- components/Internet/Resume.php
+++ components/Internet/Resume.php
@@ -3,10 +3,11 @@
                 <div class="row g-5 align-items-center">
                     <div class="col-lg-7 wow fadeInUp" data-wow-delay="0.1s">
                         <div class="section-title position-relative mb-4 pb-4">
-                            <h1 class="mb-2">Nuestra historia</h1>
+                            <h1 class="mb-2">Descripción</h1>
                         </div>
                         <p class="mb-4">
-                        Nos constituimos en 2014 como resultado de la fusión de un grupo empresarial con más de 8 años de experiencia en el mercado. <br><br> Al detectar la creciente demanda de soluciones en TI en distintos sectores clave para el país, como el de Oil & Gas, salud, y seguridad, Integradores de Tecnología incursionó en los mismos con soluciones integrales que incorporan un amplio abanico de componentes como software, hardware, obra civil, seguridad, mantenimiento y equipo, entre muchos otros. <br><br> Somos una empresa mexicana líder en la integración de proyectos con resultados comprobados. Logramos consolidar sólidas alianzas estratégicas con empresas de enorme prestigio internacional.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ratione quis inventore nisi dolores perferendis nesciunt aspernatur enim quibusdam quia laborum eius facilis sit cumque sunt obcaecati, cum delectus error. <br> <br>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi qui sed optio provident veritatis minima nam obcaecati, aliquid accusantium reprehenderit vero, placeat quidem, nostrum ipsum sint. Autem labore suscipit culpa.
                         </p>
                     </div>
                     <div class="col-lg-5">
